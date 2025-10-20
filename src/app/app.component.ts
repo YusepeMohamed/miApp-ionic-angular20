3de769ec85be9c36cb2d350e344b77e3d4dd0e61
YusepeMohamed <espinolaj407@gmail.com>
2025-10-20 20:26:31 -0300

    Agregacion de Iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,55 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  heart,
+  heartOutline,
+  chatbubbleOutline,
+  bookmark,
+  bookmarkOutline,
+  personCircle,
+  personCircleOutline,
+  home,
+  homeOutline,
+  cafe,
+  logIn,
+  logOut,
+  star,
+  starOutline,
+  settings,
+  settingsOutline,
+  search,
+  searchOutline,
+  addCircle,
+  addCircleOutline,
+  notifications,
+  notificationsOutline,
+} from 'ionicons/icons';
+
+addIcons({
+  heart,
+  heartOutline,
+  chatbubbleOutline,
+  bookmark,
+  bookmarkOutline,
+  personCircle,
+  personCircleOutline,
+  home,
+  homeOutline,
+  cafe,
+  logIn,
+  logOut,
+  star,
+  starOutline,
+  settings,
+  settingsOutline,
+  search,
+  searchOutline,
+  addCircle,
+  addCircleOutline,
+  notifications,
+  notificationsOutline,
+});
 
 @Component({
   selector: 'app-root',
